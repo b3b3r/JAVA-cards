113b6e68dfabb9dad662b6ae26f2542c1c917912
b3b3r <b3b3rseg@gmail.com>
2021-04-05 12:39:31 +0200

    edit card 32

--- src/Card/Card32.java
+++ src/Card/Card32.java
@@ -8,21 +8,6 @@ public class Card32 extends ClassicCard {
     super(figure, suit, "<3");
   }
   public static Boolean is32(Figure figure){
-    Boolean isValid = true;
-
-    switch (figure) {
-      case TWO :
-      case THREE:
-      case FOUR:
-      case FIVE:
-      case SIX:
-        isValid = false;
-        break;
-      default:
-        isValid = true;
-        break;
-    }
-
-    return isValid;
+    return figure.getorder() <= 8;
   }
 }
--- src/Enum/Figure.java
+++ src/Enum/Figure.java
@@ -1,15 +1,22 @@
 package Enum;
 
 public enum Figure {
-  ACE("AS"), KING("K"), QUEEN("Q"), VALET("V"), TEN("10"), NINE("9"), EIGHT("8"),
-  SEVEN("7"), SIX("6"), FIVE("5"), FOUR("4"), THREE("3"), TWO("2");
+  ACE("AS", 1), KING("K", 2), QUEEN("Q", 3), VALET("V", 4), TEN("10", 5), NINE("9", 6), EIGHT("8", 7),
+  SEVEN("7", 8), SIX("6", 9), FIVE("5", 10), FOUR("4", 11), THREE("3", 12), TWO("2", 13);
 
-  private String label;
-  private Figure(String label){
+  private final String label;
+  private final int order;
+  
+  private Figure(String label, int order){
     this.label = label;
+    this.order = order;
   }
 
   public String getLabel() {
     return this.label;
+  }
+
+  public int getorder() {
+    return this.order;
   }
 }
